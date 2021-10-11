ec0673d32879e6cae15fb752841dfafd7a2e714c
Xenia Filippova <32355526+riva-sm@users.noreply.github.com>
2021-10-12 01:31:54 +0800

    адаптив(неполный), fancybox

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
+
 let cost = 250; // стоимость билета
 let totalPrice = 0; // итоговая цена, изначально 0
 
@@ -11,4 +14,8 @@ schemeSvg.addEventListener("click", (event) => {
     totalPrice = totalSeats * cost;
     totalPriceTag.textContent = totalPrice; // записываем итоговую цену
   }
+});
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("is-open"); // класс, когда показывать меню
 });
